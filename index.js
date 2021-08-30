44e1d8b18bd1334766f301c233395502367604d2
Leon <hualiang.tian@gmail.com>
2021-08-29 21:41:43 -0400

    commit tree c07212f9ffc59a9c745cc37b1ef5c414459c448e

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+document.body.removeChild(main)
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+
+let victory = document.createElement('id')
+newHeader.id = "victory"
+newHeader.appendChild(victory)
+
+let message = document.createTextNode("Leon is the champion") 
+
+
+
+victory.appendChild(message)
